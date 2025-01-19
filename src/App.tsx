d23274a98fa1cdb72db5b237113ea980dd7a15b3
Anthokhiya Nath <pronoynath890@gmail.com>
2025-01-20 00:24:47 +0600

    roture with boiler added

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import MainLayout from "./components/layout/MainLayout"
+import './index.css'
+
+function App() {
+
+  return (
+    <div>
+      <MainLayout/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,66 @@
+import { Layout, Menu, MenuProps } from "antd";
+import {
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import React, { useState } from "react";
+const { Header, Content, Footer, Sider } = Layout;
+
+const items = [
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  UserOutlined,
+].map((icon, index) => ({
+  key: String(index + 1),
+  icon: React.createElement(icon),
+  label: `nav ${index + 1}`,
+}));
+
+const MainLayout = () => {
+    const [collapsed, setCollapsed] = useState(false);
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Sider
+        collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}
+      >
+        <div
+          style={{
+            color: "white",
+            height: "4rem",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>PH University</h1>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["4"]}
+          items={items}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0 }} />
+        <Content style={{ margin: "24px 16px 0" }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+            }}
+          >
+            sfdsfs
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default MainLayout;
